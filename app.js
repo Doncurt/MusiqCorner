d0e4cf46549b7bcf1498a61b35d1e26ef23a6c94
Donovan Adams <donovanadams@Donovans-MacBook-Pro.local>
2017-10-07 23:11:54 -0700

    new information on comments for the first resource

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-// set up for the
+// set up for the sever middleware, ect
 var express = require('express')
 var app = express()
 var exphbs  = require('express-handlebars');
+var mongoose = require('mongoose');
 // root route
 app.get('/', function (req, res) {
   res.render('home', {msg: 'Welcome to MusiqCorner!'});
@@ -10,7 +11,8 @@ app.get('/', function (req, res) {
 app.listen(3000, function () {
   console.log('Portfolio App listening on port 3000!')
 })
-
+//Mongoos connect function for server
+mongoose.connect('mongodb://localhost/MusiqCorner');
 
 // app engine and middleweare for handle bars
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
