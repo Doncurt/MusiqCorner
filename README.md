13ba0fad45c65a85fe8d3fe3f5a21a5fe22e45ae
Donovan Adams <donovanadams@Donovans-MacBook-Pro.local>
2017-10-15 00:41:40 -0700

    Another commit after finilising cooments

--- README.md
+++
@@ -1,2 +0,0 @@
-# MusiqCorner
-* Portfolio project to be a full fledged website to be launched in 2018 for Voice And Music students / professions to get feed back on their progress as well as ratings on performances
--- app.js
+++ app.js
@@ -1,26 +1,78 @@
-// set up for the sever middleware, ect
+//set up for the express middlewear
 var express = require('express')
+// method requirement for the overide for post on the functions
+var methodOverride = require('method-override')
 var app = express()
-var exphbs  = require('express-handlebars');
-var mongoose = require('mongoose');
+// get routes for pages, severs and other resoruces
+// OUR MOCK ARRAY OF PROJECTS
 
-// root route
+// INDEX
+
 app.get('/', function (req, res) {
-  Review.find(function(err, reviews) {
-    res.render('submissions-index', {reviews: reviews});
+  Comment.find(function(err, comments) {
+    res.render('submission-index', {comments: comments});
   })
 })
-
-app.listen(4000, function () {
-  console.log('Voice submissions App Listening on port 4000!')
+// New route for the new comments on the page
+app.get('/comments/new', function (req, res) {
+  res.render('comments-new', {});
+})
+// listener to test that the ports are open
+app.listen(3000, function () {
+  console.log('MusicCorner listening on port 3000!')
 })
 
-// app engine and middleweare for handle bars
+//templating engine code got handlebars.js
+var exphbs  = require('express-handlebars');
+
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
-//Mongoose connect function for server
-mongoose.connect('mongodb://localhost/MusiqCorner');
-var Review = mongoose.model('Review', {
-  title: String
+//mongoose for document object maping
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/MusicCorner');
+// model for mongoose to handle data
+var Comment = mongoose.model('Comment', {
+  name: String,
+  comment: String
 });
+// INITIALIZE BODY-PARSER AND ADD IT TO APP
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// CREATE
+app.post('/comments', function (req, res) {
+  Comment.create(req.body, function(err, comments) {
+    res.redirect('/comments/' + comments._id);
+  })
+})
+
+// SHOW
+app.get('/comments/:id', function (req, res) {
+  Comment.findById(req.params.id).exec(function (err, comments) {
+    res.render('comments-show', {comments: comments});
+  })
+});
+
+// EDIT
+app.get('/comments/:id/edit', function (req, res) {
+  Comment.findById(req.params.id, function(err, comments) {
+    res.render('comments-edit', {comments: comments});
+  })
+})
+// override with POST having ?_method=DELETE or ?_method=PUT
+app.use(methodOverride('_method'))
+
+// UPDATE
+app.put('/comments/:id', function (req, res) {
+  Comment.findByIdAndUpdate(req.params.id,  req.body, function(err, comments) {
+    res.redirect('/comments/' + comments._id);
+  })
+})
+
+// DELETE
+app.delete('/comments/:id', function (req, res) {
+  Comment.findByIdAndRemove(req.params.id, function(err) {
+    res.redirect('/');
+  })
+})
